3921c3e6683b2c0f206f48d7ffd77305c45953b4
MD. RISHADUL ISLAM KHAN <rishadulislam@outlook.com>
2019-01-26 10:19:06 +0600

    have some problems in newto raphson

--- Newton-RaphsonTest/src/newton/raphsontest/NewtonRaphsonTest.java
+++ Newton-RaphsonTest/src/newton/raphsontest/NewtonRaphsonTest.java
@@ -5,32 +5,56 @@ import java.util.Scanner;
 
 public class NewtonRaphsonTest {
     
+    private static final double E = 0.0001;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the degree: ");
         int degree = input.nextInt();
-        int[] equation = new int[degree];
+        double[] equation = new double[degree];
         
-        System.out.println("enter the values");
+        System.out.println("Enter the values: ");
         for(int i = 0; i < equation.length; i++){
-            equation[i] = input.nextInt();
+            equation[i] = input.nextDouble();
         }
         
         System.out.println("Enter the initial assumption: ");
-        int assumption = input.nextInt();
+        double x0 = input.nextDouble();
         
-        int hor = horners(equation, assumption);
-        System.out.println("Horners: " + hor);
+        double hor = horners(equation, x0);
+        //System.out.println("Horners: " + hor);
         
-        int[] diff = differenciate(equation, degree);
-        for(int n : diff){
-            System.out.println(n);
-        }
+        double[] differentiation = differenciate(equation, degree);
+  
+//        System.out.println("equation value: " + horners(equation, x0));
+//        System.out.println("differentiation value: " + horners(differentiation, x0));
+        
+        double x1;
+        do{
+            if(horners(equation, x0) == 0){
+                System.out.println("root is: " + x0);
+                break;
+            }
+            double h = horners(equation, x0) / horners(differentiation, x0);
+            //System.out.println(h);
+            x1 = x0 - h;
+            //System.out.println(x1);
+            if(horners(equation, x1) == 0.0){
+                System.out.println("Root is: " + x1);
+                break;
+            }else{
+                x0 = x1;
+            }
+        }while(Math.abs((x1 - x0)/ x1) < E);
+        
+        //System.out.println("root is: " + x1);
+    }
+    
+    public static void newtonRaphson(int[] equation, int[] differentiation){
         
     }
     
-    public static int horners(int[] equation, int inputValue){
-        int result = equation[0];
+    public static double horners(double[] equation, double inputValue){
+        double result = equation[0];
         int length = equation.length;
         for(int i = 1; i < length; i++){
             result = result * inputValue + equation[i];
@@ -38,9 +62,9 @@ public class NewtonRaphsonTest {
         return result;
     }
     
-    public static int[] differenciate(int[] equation, int degree){
+    public static double[] differenciate(double[] equation, int degree){
         int length = equation.length;
-        int[] diffArray = new int[length - 1];
+        double[] diffArray = new double[length - 1];
         
         for(int i = 1; i < length; i++){
             diffArray[i - 1] = equation[i - 1] * --degree;
