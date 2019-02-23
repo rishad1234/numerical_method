c70290183bc45631f726e5209d562f3310f5cddd
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-23 11:35:17 +0600

    equation initialization is done

--- Synthetic_Division/src/synthetic_division/Synthetic_Division.java
+++ Synthetic_Division/src/synthetic_division/Synthetic_Division.java
@@ -1,10 +1,40 @@
 
 package synthetic_division;
 
+import java.util.Scanner;
+
 public class Synthetic_Division {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of degree: ");
+        int degree = input.nextInt();
+        int[] equation = new int[degree];
         
+        System.out.println("Enter the values: ");
+        for(int i = equation.length - 1; i >= 0; i--){
+            equation[i] = input.nextInt();
+        }
+        
+        System.out.println("the equation is: ");
+        for(int i = equation.length - 1; i >= 0; i--){
+            if(i == 0){
+                System.out.print(equation[i]);
+            }else{
+                System.out.print(equation[i] + "x^" + i);
+                checkForSign(equation[i]);
+            }
+        }
+        System.out.print("=0");
+        System.out.println("");
+    }
+    
+    private static void checkForSign(int number){
+        if(number < 0){
+            System.out.print("+");
+        }else{
+            System.out.print("");
+        }
     }
     
 }
