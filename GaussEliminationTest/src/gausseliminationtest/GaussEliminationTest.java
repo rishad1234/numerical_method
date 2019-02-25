652ac95386f4a2f3f7032bb43b18940c1d4f74db
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-25 13:21:26 +0600

    gauss elimination refactored

--- GaussEliminationTest/src/gausseliminationtest/GaussEliminationTest.java
+++ GaussEliminationTest/src/gausseliminationtest/GaussEliminationTest.java
@@ -5,65 +5,65 @@ import java.util.Scanner;
 
 public class GaussEliminationTest {
 
-    public static double[][] a = new double[10][10];
-    public static double[] b = new double[10];
-    public static double[] x = new double[10];
+    public static double[][] coefficients = new double[10][10];
+    public static double[] rightSideVector = new double[10];
+    public static double[] resultVector = new double[10];
     public static int status = 0;
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int n;
+        int size;
         System.out.println("Enter the size of the system: ");
-        n = input.nextInt();
+        size = input.nextInt();
         System.out.println("one row at a time");
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                a[i][j] = input.nextDouble();
+        for(int i = 1; i <= size; i++){
+            for(int j = 1; j <= size; j++){
+                coefficients[i][j] = input.nextDouble();
             }
         }
         
         System.out.println("Enter the input vectors: ");
-        for(int i = 1; i <= n; i++){
-            b[i] = input.nextDouble();
+        for(int i = 1; i <= size; i++){
+            rightSideVector[i] = input.nextDouble();
         }
         
-        gauss(n);
+        gauss(size);
         
         if(status != 0){
             System.out.println("Solution vector: ");
-            for(int i = 1; i <= n; i++){
-                System.out.println(x[i]);
+            for(int i = 1; i <= size; i++){
+                System.out.println(resultVector[i]);
             }
         }else{
             System.out.println("Singular matrix, reorder equations");
         }
     }
     
-    public static void gauss(int n){
+    public static void gauss(int size){
         double pivot, factor, sum;
         
-        for(int k = 1; k <= n - 1; k++){
-            pivot = a[k][k];
+        for(int k = 1; k <= size - 1; k++){
+            pivot = coefficients[k][k];
             if(pivot < 0.000001){
                 status = 0;
                 return;
             }
             status = 1;
-            for(int i = k + 1; i <= n; i++){
-                factor = a[i][k] / pivot;
-                for(int j = k + 1; j <= n; j++){
-                    a[i][j] = a[i][j] - factor * a[k][j];
+            for(int i = k + 1; i <= size; i++){
+                factor = coefficients[i][k] / pivot;
+                for(int j = k + 1; j <= size; j++){
+                    coefficients[i][j] = coefficients[i][j] - factor * coefficients[k][j];
                 }
-                b[i] = b[i] - factor * b[k];
+                rightSideVector[i] = rightSideVector[i] - factor * rightSideVector[k];
             }
         }
         
-        x[n] = b[n] / a[n][n];
-        for(int k = n - 1; k >= 1; k--){
+        resultVector[size] = rightSideVector[size] / coefficients[size][size];
+        for(int k = size - 1; k >= 1; k--){
             sum = 0.0;
-            for(int j = k + 1; j <= n; j++){
-                sum = sum + a[k][j] * x[j];
+            for(int j = k + 1; j <= size; j++){
+                sum = sum + coefficients[k][j] * resultVector[j];
             }
-            x[k] = (b[k] - sum) / a[k][k];
+            resultVector[k] = (rightSideVector[k] - sum) / coefficients[k][k];
         }
     }
 }
