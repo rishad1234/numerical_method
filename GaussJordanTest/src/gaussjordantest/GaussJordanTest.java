a2175d7526dbd71469652e0a7c5b57ff8bd2fc05
Rishadul Islam <rishadulislamkhan@gmail.com>
2019-02-26 08:28:23 +0600

    gauss jordan implemented

--- GaussJordanTest/src/gaussjordantest/GaussJordanTest.java
+++ GaussJordanTest/src/gaussjordantest/GaussJordanTest.java
@@ -1,21 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gaussjordantest;
 
-/**
- *
- * @author Admin
- */
-public class GaussJordanTest {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class GaussJordanTest {
+    public static double[][] ar = new double[100][100];
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter the size of the system: ");
+        int n = input.nextInt();
+        System.out.println("Enther the coefficients: ");
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n + 1; j++){
+                ar[i][j] = input.nextDouble();
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            double x = ar[i][i];
+            
+            for(int j = 0; j < n + 1; j++){
+                ar[i][j] = ar[i][j] / x;
+            }
+            
+            for(int j = 0; j < n; j++){
+                if(i == j){
+                    continue;
+                }
+                double temp = ar[j][i];
+                
+                for(int k = 0; k < n + 1; k++){
+                    ar[j][k] = ar[j][k] - ar[i][k] * temp;
+                }
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            System.out.println(ar[i][n]);
+        }
     }
-    
 }
