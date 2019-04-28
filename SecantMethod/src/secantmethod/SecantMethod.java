39b3c3d40a0c7647eb596f6ff2ab5f6a22e7161d
MD. RISHADUL ISLAM KHAN <rishadulislam@outlook.com>
2019-04-28 22:36:35 +0600

    Update SecantMethod.java

--- SecantMethod/src/secantmethod/SecantMethod.java
+++ SecantMethod/src/secantmethod/SecantMethod.java
@@ -28,12 +28,17 @@ public class SecantMethod {
         
         while(true){
             double x3 = (f2 * x1 - f1 * x2) / (f2 - f1);
+            System.out.println(x1 + "\t\t\t" + x2 + "\t\t\t" + x3 + "\t\t\t" + f1 + "\t\t\t" + f2);
+                    
             if(Math.abs((x3 - x2)/ x3) > E){
+                System.out.println(Math.abs((x3 - x2)/ x3));
                 x1 = x2;
                 f1 = f2;
                 x2 = x3;
                 f2 = horners(equation, x3);
+                System.out.println(f2);
             }else{
+                System.out.println(Math.abs((x3 - x2)/ x3));
                 System.out.println("Root is: " + x3);
                 break;
             }
